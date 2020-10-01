2259e7315d1186fcf40f9dd7a1e3aee45aabbbd4
JaNigam <69103142+JaNigam@users.noreply.github.com>
2020-10-01 11:24:28 +0530

    added model for handling posts in blog

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 # Create your models here.
+
+class post(models.Model):
+    SrNo = models.AutoField(primary_key = True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    slug = models.SlugField(max_length = 200)
+    timeStamp = models.DateTimeField(blank =  True)
+
+    def __str__(self):
+        return self.title + " by " + self.author
